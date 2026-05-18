d65a4daf30832c6a477fc303d9115fb7a46890ee
unknown <antoine.cosne@ik.me>
2026-05-18 12:01:34 +0200

    Backup before reforging whole program (dissociation of widgets, functions and data).

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import background_fetch
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    BackgroundFetchBackgroundFetch.register()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
